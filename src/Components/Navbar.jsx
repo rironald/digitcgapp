52c9ecac81776de002b1620bf7388a5c636a85af
Ronald Rivera <rironald3@gmail.com>
2024-03-06 00:36:11 -0800

    Added routes and styling to sign-in page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { VscAccount } from "react-icons/vsc"
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -10,13 +12,13 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center h-24 max-w-[1240px] mx-auto px-4'>
-        <h1 className='w-full text-[#00408b] text-3xl font-bold'>Digi-TCG</h1>
+        <h1 className='w-full text-[#00408b] text-3xl font-bold'><NavLink to="/">Digi-TCG</NavLink></h1>
         <ul className='hidden md:flex'>
-            <li className='p-4'>Home</li>
+            <li className='p-4'><NavLink to="/">Home</NavLink></li>
             <li className='p-4'>Cards</li>
             <li className='p-4'>Decks</li>
             <li className='p-4'>News</li>
-            <li className='p-4'>Account</li>
+            <li className='px-2 py-3'><NavLink to="Login"><VscAccount className='inline' size={30}/></NavLink></li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu />}
@@ -28,7 +30,7 @@ const Navbar = () => {
             <li className='p-4 border-b border-b-gray-600'>Cards</li>
             <li className='p-4 border-b border-b-gray-600'>Decks</li>
             <li className='p-4 border-b border-b-gray-600'>News</li>
-            <li className='p-4'>Account</li>
+            <li className='p-4'>Sign-In</li>
           </ul>
         </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/tailwind.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './Pages/Login.jsx'
+
+const router = createBrowserRouter([
+  {
+  path: '/',
+  element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
