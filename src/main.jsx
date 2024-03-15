c05c3f6af5029e683a2c2ab7fcef089b8dd15fd1
Ronald Rivera <rironald3@gmail.com>
2024-03-15 01:00:03 -0700

    Created Account Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/tailwind.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './Pages/Login.jsx'
-import CardDB from './Pages/CardDB.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/tailwind.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Pages/Login.jsx";
+import CardDB from "./Pages/CardDB.jsx";
+import Profile from "./Pages/Profile.jsx";
 
 const router = createBrowserRouter([
   {
-  path: '/',
-  element: <App />,
+    path: "/",
+    element: <App />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/cards',
+    path: "/cards",
     element: <CardDB />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
