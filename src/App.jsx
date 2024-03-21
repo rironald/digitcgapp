78d6b469722b372f8dce1545e617caa7e8356d75
Ronald Rivera <rironald3@gmail.com>
2024-03-21 01:15:08 -0700

    Configured process env in vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Latest from "./Components/Latest";
 import Newsletter from "./Components/Newsletter";
 import UpcomingEvents from "./Components/UpcomingEvents";
 
-import { collection, getDoc, getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../src/config/firebase";
 
 function App() {
