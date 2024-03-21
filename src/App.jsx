511a8eb8d065244635be7d98ebe7d8730dbd83de
Ronald Rivera <rironald3@gmail.com>
2024-03-21 01:00:32 -0700

    Added UseEffect hook to pull data from firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import Navbar from './Components/Navbar'
-import Footer from './Components/Footer'
-import Contact from './Components/Contact'
-import Hero from './Components/Hero'
-import Latest from './Components/Latest'
-import Newsletter from './Components/Newsletter'
-import UpcomingEvents from './Components/UpcomingEvents'
+import { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Contact from "./Components/Contact";
+import Hero from "./Components/Hero";
+import Latest from "./Components/Latest";
+import Newsletter from "./Components/Newsletter";
+import UpcomingEvents from "./Components/UpcomingEvents";
+
+import { collection, getDoc, getDocs } from "firebase/firestore";
+import { db } from "../src/config/firebase";
 
 function App() {
+  const [digimon, setDigimon] = useState();
+
+  const getDigimon = async () => {
+    const querySnapshot = await getDocs(collection(db, "cardDB"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+    setDigimon(digimon);
+  };
+
+  useEffect(() => {
+    getDigimon();
+  }, []);
 
   return (
     <>
@@ -21,7 +38,7 @@ function App() {
         <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/CardDB.jsx
+++ src/Pages/CardDB.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
-import Footer from '../Components/Footer'
+import React, { useEffect } from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const CardDB = () => {
   return (
     <div>
-        <div>
-            <Navbar />
-        </div>
-        <div>
-          <h1>hello</h1>
-        </div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <h1>hello</h1>
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default CardDB
+export default CardDB;
